530af47ef228c1f8d7c7c39537c8c026a2b7dc05
saurabh-ssingh <target4ever101@gmail.com>
2024-03-15 11:48:18 +0530

    authenticate an user

---
+++ src/main/java/com/userManagement/userManagement/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,21 @@
+package com.userManagement.userManagement.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+        throws IOException, ServletException, IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        PrintWriter writer = response.getWriter();
+        writer.println("Access Denied !! Message :" + authException.getMessage());
+    }
+}
--- src/main/java/com/userManagement/userManagement/security/SecurityConfig.java
+++ src/main/java/com/userManagement/userManagement/security/SecurityConfig.java
@@ -1,13 +1,15 @@
 package com.userManagement.userManagement.security;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,32 +17,38 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
   @Autowired
   private UserDetailsService userDetailsService;
 
+  @Autowired
+  private JwtAuthenticationEntryPoint point;
 
   /**
-   * Provides a bean for configuring the security filter chain.
-   * The security filter chain defines the security configuration
-   * for HTTP requests, including CSRF protection, CORS policy,
-   * request authorization rules, and HTTP basic authentication.
+   * Provides a bean for configuring the security filter chain. The security filter chain defines
+   * the security configuration for HTTP requests, including CSRF protection, CORS policy, request
+   * authorization rules, and HTTP basic authentication.
    *
-   * @param http The {@link HttpSecurity} object used to configure
-   * security settings for HTTP requests.
-   * @return A {@link SecurityFilterChain} instance representing
-   * the configured security filter chain for the application.
+   * @param http The {@link HttpSecurity} object used to configure security settings for HTTP
+   *             requests.
+   * @return A {@link SecurityFilterChain} instance representing the configured security filter
+   * chain for the application.
    * @throws Exception If an error occurs during the security configuration.
    */
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.csrf(AbstractHttpConfigurer::disable)
-        .cors(AbstractHttpConfigurer::disable)
+    http.csrf(csrf -> csrf.disable())
+        .cors(cors -> cors.disable())
+        .authenticationProvider(daoAuthenticationProvider())
         .authorizeHttpRequests(auth ->
-            auth.requestMatchers(HttpMethod.POST,"/user/create").permitAll()
+            auth.requestMatchers(HttpMethod.POST, "/user/create/**").permitAll()
                 .anyRequest().authenticated())
-        .httpBasic(Customizer.withDefaults());
+        .exceptionHandling(ex -> ex.authenticationEntryPoint(point))
+        .httpBasic(withDefaults())
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
     return http.build();
   }
 
@@ -99,5 +107,4 @@ public class SecurityConfig {
     provider.setPasswordEncoder(passwordEncoder());
     return provider;
   }
-
 }
